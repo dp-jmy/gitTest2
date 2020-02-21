5f7ab44c1ffe0280fc8e2a128f5b860168a7d78c
dp-jmy <61302733+dp-jmy@users.noreply.github.com>
2020-02-21 14:55:03 +0900

    Update gitTest2.java

--- src/gitTest2/gitTest2.java
+++ src/gitTest2/gitTest2.java
@@ -2,7 +2,7 @@ package gitTest2;
 
 public class gitTest2 {
 	public static void main(String[] args) {
-		System.out.println("sdfdsf2222222222222");
+		System.out.println("sdfdsf222222222222299999999999999");
 		System.out.println("sdfdsf");
 	}
 }
